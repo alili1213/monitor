392055f2dffc4a53510cd1a063ff6233ce458ab8
邱泽寰 <qiuzehuan@corp.netease.com>
2022-10-01 16:26:38 +0800

    feat:init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "monitor",
-	version = "0.1",
+	version = "0.2",
 	url = 'https://github.com/alili1213/monitor.git',
 	long_description = open('README.md').read(),
 	packages = find_packages(),
