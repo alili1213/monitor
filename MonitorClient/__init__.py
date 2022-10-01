62b5b106257af582a00ef15a4a802a1cf5372d65
邱泽寰 <qiuzehuan@corp.netease.com>
2022-10-01 16:33:48 +0800

    feat:init

--- MonitorClient/__init__.py
+++ MonitorClient/__init__.py
@@ -1,5 +1,19 @@
-def post_data():
+import socket
+
+
+def post_data(a):
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    print(a)
+    print(host)
+    print(ip)
     print('post_data')
 
-def post_data_interval():
+
+def post_data_interval(a):
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    print(a)
+    print(host)
+    print(ip)
     print('post_data_interval')
